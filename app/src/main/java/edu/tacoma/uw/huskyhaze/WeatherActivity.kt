5a646d8029a8ce3f3e74ffbc39bad8a481d89c6b
Taylor <taylor.merwin@gmail.com>
2024-04-24 16:38:33 -0700

    Created Weather and News Activities

---
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package edu.tacoma.uw.huskyhaze
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
