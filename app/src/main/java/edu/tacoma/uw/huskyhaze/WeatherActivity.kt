9cd982855950110d77db6a7cbee93a1dcc549604
Taylor <taylor.merwin@gmail.com>
2024-04-24 23:03:38 -0700

    Weather API is called on loading Weather Activity and the current temperature is logged.

--- app/src/main/java/edu/tacoma/uw/huskyhaze/WeatherActivity.kt
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/WeatherActivity.kt
@@ -1,12 +1,52 @@
 package edu.tacoma.uw.huskyhaze
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import edu.tacoma.uw.huskyhaze.network.WeatherService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class WeatherActivity : AppCompatActivity() {
+    private lateinit var temperatureTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        temperatureTextView = findViewById(R.id.temperatureTextView)
+
+        fetchCurrentWeather()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun fetchCurrentWeather() {
+        val weatherService = WeatherService.create()
+        val apiKey = getString(R.string.open_weather_api_key)
+        val latitude = 47.24
+        val longitude = -122.43
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response =
+                weatherService.getCurrentWeatherByCoordinates(latitude, longitude, apiKey)
+
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    val weatherData = response.body()
+                    temperatureTextView.text = "Temperature: $weatherData"
+                    Log.d("WeatherResponse", weatherData.toString())
+                } else {
+                    Log.e("WeatherResponse", response.errorBody().toString())
+                    temperatureTextView.text = "Failed to get weather data"
+                }
+
+            }
+
+
+        }
     }
 }
