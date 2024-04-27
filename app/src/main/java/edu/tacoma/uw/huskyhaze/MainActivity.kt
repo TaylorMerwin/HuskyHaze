3800e448ff313daf06c0e52c8d41f0fc34e15a6f
William <whay@uw.edu>
2024-04-27 14:42:07 -0700

    Added temporary setting icon, added listener to the icon, and placed the icon on the main activity in the top right for testing.

--- app/src/main/java/edu/tacoma/uw/huskyhaze/MainActivity.kt
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.tacoma.uw.huskyhaze
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         val weatherButton = findViewById<Button>(R.id.weatherButton)
         val newsButton = findViewById<Button>(R.id.newsButton)
+        val settingsButton = findViewById<ImageButton>(R.id.settingsButton)
 
         weatherButton.setOnClickListener {
             val intent = Intent(this, WeatherActivity::class.java)
@@ -24,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         newsButton.setOnClickListener {
             val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
     }
