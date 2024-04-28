a17ed46d68db3fd14fed5c729487dab0555f19d0
Taylor <taylor.merwin@gmail.com>
2024-04-27 22:11:47 -0700

    Implemented Forecast data for daily, min, max temp for 8 days

--- app/src/main/java/edu/tacoma/uw/huskyhaze/models/WeatherData.kt
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/models/WeatherData.kt
@@ -1,6 +1,9 @@
 package edu.tacoma.uw.huskyhaze.models
 
 class WeatherData {
-    data class WeatherData(val main: MainData)
-    data class MainData(val temp: Double)
+    data class WeatherData(val current: CurrentData, val daily: List<DailyData>)
+    data class CurrentData(val temp: Double)
+    data class DailyData(val temp: TempData)
+
+    data class TempData(val day: Double, val min: Double, val max: Double)
 }
