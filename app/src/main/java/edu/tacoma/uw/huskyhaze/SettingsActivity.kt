02354f0b6aa0fc212e5eac31d17ba445a06b4dd5
Minh Vu <minhv2@uw.edu>
2024-04-27 23:04:54 -0700

    Implemented Dark/Light Mode

--- app/src/main/java/edu/tacoma/uw/huskyhaze/SettingsActivity.kt
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/SettingsActivity.kt
@@ -1,26 +1,47 @@
 package edu.tacoma.uw.huskyhaze
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var switcher: Switch
+    private var nightMode = false
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        supportActionBar?.hide()
+        switcher = findViewById(R.id.dark_light_switch)
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
+        nightMode = sharedPreferences.getBoolean("night", false)
+
+        if (nightMode) {
+            switcher.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        switcher.setOnClickListener {
+            if (nightMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor = sharedPreferences.edit()
+                editor.putBoolean("night", false)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor = sharedPreferences.edit()
+                editor.putBoolean("night", true)
+            }
+            editor.apply()
         }
     }
 }
