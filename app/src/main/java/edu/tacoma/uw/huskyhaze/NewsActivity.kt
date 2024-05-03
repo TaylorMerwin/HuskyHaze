da82e39632cc544baafa0d5f275321a60618bfb9
William <whay@uw.edu>
2024-05-03 15:48:05 -0700

    Added UI changes to news activity that adds category buttons along with a search bar, intents and functionality still WIP.

--- app/src/main/java/edu/tacoma/uw/huskyhaze/NewsActivity.kt
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/NewsActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,12 +14,36 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class NewsActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity(), View.OnClickListener {
+
+    var recyclerView: RecyclerView? = null
+    var adapter: NewsAdapter? = null
+    var b1: Button? = null
+    var b2: Button? = null
+    var b3: Button? = null
+    var b4: Button? = null
+    var b5: Button? = null
+    var b6: Button? = null
+    var b7: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
         fetchNewsHeadlines()
+        b1 = findViewById(R.id.btn_1)
+        b1?.setOnClickListener(this)
+        b2 = findViewById(R.id.btn_2)
+        b2?.setOnClickListener(this)
+        b3 = findViewById(R.id.btn_3)
+        b3?.setOnClickListener(this)
+        b4 = findViewById(R.id.btn_4)
+        b4?.setOnClickListener(this)
+        b5 = findViewById(R.id.btn_5)
+        b5?.setOnClickListener(this)
+        b6 = findViewById(R.id.btn_6)
+        b6?.setOnClickListener(this)
+        b7 = findViewById(R.id.btn_7)
+        b7?.setOnClickListener(this)
     }
 
     private fun fetchNewsHeadlines() {
@@ -50,5 +76,13 @@ class NewsActivity : AppCompatActivity() {
                 Log.e("NewsResponse", response.errorBody().toString())
             }
         }
+    }
+
+    override fun onClick(v: View?) {
+        val button = v as Button
+        val category = button.getText().toString()
+        // Code below is from a tutorial, will look at this later.
+//        val manager = RequestManager(this)
+//        manager.getNewsHeadlines(listener, category, null)
     }
 }
