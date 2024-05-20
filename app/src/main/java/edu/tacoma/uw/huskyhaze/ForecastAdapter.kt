212f9324ebae1cb8c599a270f816840e1ca399ca
Keegan Sanders <46981601+sanderskn11@users.noreply.github.com>
2024-05-20 11:23:24 -0700

    weather activity

--- app/src/main/java/edu/tacoma/uw/huskyhaze/ForecastAdapter.kt
+++ app/src/main/java/edu/tacoma/uw/huskyhaze/ForecastAdapter.kt
@@ -1,14 +1,17 @@
 package edu.tacoma.uw.huskyhaze
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import edu.tacoma.uw.huskyhaze.models.DateUtils.formatToDayMonth
 import edu.tacoma.uw.huskyhaze.models.DateUtils.toDate
 import edu.tacoma.uw.huskyhaze.models.WeatherData
+import java.util.Calendar
 import kotlin.math.roundToInt
 
 class ForecastAdapter(private val forecastList: List<WeatherData.DailyData>) :
@@ -19,6 +22,7 @@ class ForecastAdapter(private val forecastList: List<WeatherData.DailyData>) :
         val minTempTextView: TextView = itemView.findViewById(R.id.minTempTextView)
         val maxTempTextView: TextView = itemView.findViewById(R.id.maxTempTextView)
         val weatherTypeTextView: TextView = itemView.findViewById(R.id.weatherTypeTextView)
+        val weatherTypeImageView: ImageView = itemView.findViewById(R.id.weatherTypeImageView)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ForecastViewHolder {
         val itemView = LayoutInflater.from(parent.context)
@@ -26,15 +30,38 @@ class ForecastAdapter(private val forecastList: List<WeatherData.DailyData>) :
         return ForecastViewHolder(itemView)
     }
 
-        @SuppressLint("SetTextI18n")
-        override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
-            val currentForecast = forecastList[position]
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
+        val currentForecast = forecastList[position]
 
-            holder.dateTextView.text = currentForecast.dt.toDate().formatToDayMonth()
-            holder.minTempTextView.text = "Min: " + currentForecast.temp.min.roundToInt().toString() + "°F"
-            holder.maxTempTextView.text = "Max: " + currentForecast.temp.max.roundToInt().toString() + "°F"
-            holder.weatherTypeTextView.text = currentForecast.weather[0].main
+        holder.dateTextView.text = currentForecast.dt.toDate().formatToDayMonth()
+        holder.minTempTextView.text = "Min: " + currentForecast.temp.min.roundToInt().toString() + "°F"
+        holder.maxTempTextView.text = "Max: " + currentForecast.temp.max.roundToInt().toString() + "°F"
+        holder.weatherTypeTextView.text = currentForecast.weather[0].main
+        holder.weatherTypeImageView.setImageResource(getWeatherIcon(currentForecast.weather[0].main))
+    }
+
+    private fun getWeatherIcon(weatherType: String): Int {
+        return when (weatherType) {
+            "Clear" -> getTimeOfDayForClear()
+            in "Clouds" -> R.drawable.ic_clouds
+            in "Atmosphere" -> R.drawable.ic_atmosphere
+            in "Snow" -> R.drawable.ic_snow
+            in "Rain" -> R.drawable.ic_rain
+            in "Drizzle" -> R.drawable.ic_drizzle
+            in "Thunderstorm" -> R.drawable.ic_thunderstorm
+            else -> getTimeOfDayForClear()
         }
+    }
+
+    private fun getTimeOfDayForClear(): Int {
+        val hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+
+        return when (hourOfDay) {
+            in 6..18 -> R.drawable.ic_clear_day  // 6 AM to 6 PM
+            else -> R.drawable.ic_clear_night // 7 PM to 5AM
+        }
+    }
 
     override fun getItemCount(): Int {
         return forecastList.size
